51ef0998ed9f8657190a5856179cedcbe4c3c401
tameem <hmk.tameem@gmail.com>
2024-05-26 01:16:23 +0400

    chi router and middleware logger

--- main.go
+++ main.go
@@ -4,12 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
+var counter int = 0
 
 func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/hello", basicHandler)
 	server := &http.Server{
 		Addr: ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}	
 	defer server.Close()
 	err := server.ListenAndServe()
@@ -19,12 +26,8 @@ func main() {
 }
 
 
-func DisplayData(r *http.Request) {
-	fmt.Printf("METHOD: %v\n", r.Method)
-	fmt.Printf("REQUEST: %v\n", r.URL)
-}
 
 func basicHandler (w http.ResponseWriter, r *http.Request) {
-	DisplayData(r)
-	w.Write([]byte("Hi Tameem!"))
+	counter++
+	w.Write([]byte(fmt.Sprintf("Hi Tameem! %d", counter)))
 }
