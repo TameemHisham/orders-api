fbc20cb5dd2ffe017c5652a0643f853fef7f3bd8
tameem <hmk.tameem@gmail.com>
2024-05-26 10:58:17 +0400

    Implmenting CRUD routees

---
+++ handler/order.go
@@ -0,0 +1,25 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Order struct {}
+
+func (o *Order)  Create (w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create Order")
+}
+func (o *Order)  List (w http.ResponseWriter, r *http.Request) {
+	fmt.Println("List all Orders")
+}
+
+func (o *Order)  GetByID (w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get an order by ID")
+}
+func (o *Order)  UpdateByID (w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update an order by ID")
+}
+func (o *Order)  DeleteByID (w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Delete an order by ID")
+}
--- main.go
+++ main.go
@@ -1,33 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/TameemHisham/orders-api/application"
 )
-var counter int = 0
 
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-	router.Get("/hello", basicHandler)
-	server := &http.Server{
-		Addr: ":3000",
-		Handler: router,
-	}	
-	defer server.Close()
-	err := server.ListenAndServe()
+	app := application.New()
+	err := app.Start(context.TODO())
 	if err != nil {
-		log.Fatalf("Error while Listen to Serve, Error: %v", err)
+		fmt.Printf("Failed to start app: %v\n", err)
 	}
-}
-
-
-
-func basicHandler (w http.ResponseWriter, r *http.Request) {
-	counter++
-	w.Write([]byte(fmt.Sprintf("Hi Tameem! %d", counter)))
 }
